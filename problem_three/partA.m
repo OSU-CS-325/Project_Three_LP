87f1a86daab5389cb105889125e5b840d8b8c52d
lemArts <cash.stramel@gmail.com>
2016-11-06 20:20:51 -0800

    completed Matlab code for P1A - still need to check

--- problem_three/partA.m
+++ problem_three/partA.m
@@ -1,31 +1,44 @@
+% ------------------------------------------------------------
+% reference: array index to variable mapping
+% ------------------------------------------------------------
+% (1) np11
+% (2) np12
+% (3) np21
+% (4) np22
+% (5) np31
+% (6) np32
+% (7) np33
+% (8) np42
+% (9) np43
+% (10) nw11
+% (11) nw12
+% (12) nw13
+% (13) nw14
+% (14) nw23
+% (15) nw24
+% (16) nw25
+% (17) nw26
+% (18) nw34
+% (19) nw35
+% (20) nw36
+% (21) nw37
+% (22) s1
+% (23) s2
+% (24) s3
+% (25) s4
+% (26) d1
+% (27) d2
+% (28) d3
+% (29) d4
+% (30) d5
+% (31) d6
+% (32) d7
+
+% ------------------------------------------------------------
 % lower bounds vector
 %   note matlab arrays/vectors start at index 1 (not 0)
+% ------------------------------------------------------------
 lb = zeros(32,1);
-lb(1)  = 0     % np11
-lb(2)  = 0     % np12
-lb(3)  = 0     % np21
-lb(4)  = 0     % np22
-lb(5)  = 0     % np31
-lb(6)  = 0     % np32
-lb(7)  = 0     % np33
-lb(8)  = 0     % np42
-lb(9)  = 0     % np43
-lb(10) = 0     % nw11
-lb(11) = 0     % nw12
-lb(12) = 0     % nw13
-lb(13) = 0     % nw14
-lb(14) = 0     % nw23
-lb(15) = 0     % nw24
-lb(16) = 0     % nw25
-lb(17) = 0     % nw26
-lb(18) = 0     % nw34
-lb(19) = 0     % nw35
-lb(20) = 0     % nw36
-lb(21) = 0     % nw37
-lb(22) = 0     % s1
-lb(23) = 0     % s2
-lb(24) = 0     % s3
-lb(25) = 0     % s4
 lb(26) = 100   % d1
 lb(27) = 150   % d2
 lb(28) = 100   % d3
@@ -33,8 +46,11 @@ lb(29) = 200   % d4
 lb(30) = 200   % d5
 lb(31) = 150   % d6
 lb(32) = 100   % d7
+
+% ------------------------------------------------------------
 % upper bounds vector
 %   note matlab arrays/vectors start at index 1 (not 0)
+% ------------------------------------------------------------
 ub = Inf(32,1);
 ub(1)  = 150   % np11
 ub(2)  = 150   % np12
@@ -45,66 +61,96 @@ ub(6)  = 250   % np32
 ub(7)  = 250   % np33
 ub(8)  = 150   % np42
 ub(9)  = 150   % np43
-ub(10) = Inf   % nw11
-ub(11) = Inf   % nw12
-ub(12) = Inf   % nw13
-ub(13) = Inf   % nw14
-ub(14) = Inf   % nw23
-ub(15) = Inf   % nw24
-ub(16) = Inf   % nw25
-ub(17) = Inf   % nw26
-ub(18) = Inf   % nw34
-ub(19) = Inf   % nw35
-ub(20) = Inf   % nw36
-ub(21) = Inf   % nw37
 ub(22) = 150   % s1
 ub(23) = 450   % s2
 ub(24) = 250   % s3
 ub(25) = 150   % s4
-ub(26) = Inf   % d1
-ub(27) = Inf   % d2
-ub(28) = Inf   % d3
-ub(29) = Inf   % d4
-ub(30) = Inf   % d5
-ub(31) = Inf   % d6
-ub(32) = Inf   % d7
+
+% ------------------------------------------------------------
 % linear inequality matrix and vector
 %   note matlab arrays/vectors start at index 1 (not 0)
-% linear inequality matrix and vector
+% ------------------------------------------------------------
+A = []
+b = []
+
+% ------------------------------------------------------------
+% linear equality matrix and vector
 %   note matlab arrays/vectors start at index 1 (not 0)
+%   14 equations in 32 variables
+% ------------------------------------------------------------
+Aeq = zeros(14, 32)
+beq = zeros(14, 1)
+%np11 + np21 + np31        = nw11 + nw12 + nw13 + nw14
+%np11 + np21 + np31 - nw11 - nw12 - nw13 - nw14 = 0
+Aeq(1,[1,3,5,10,11,12,13]) = [1,1,1,-1,-1,-1,-1];
+%np12 + np22 + np32 + np42 =               nw23 + nw24 + nw25 + nw26
+%np12 + np22 + np32 + np42 - nw23 - nw24 - nw25 - nw26 = 0
+Aeq(2,[2,4,6,8,14,15,16,17]) = [1,1,1,1,-1,-1,-1,-1];
+%              np33 + np43 =                      nw34 + nw35 + nw36 + nw37
+%np33 + np43 - nw34 - nw35 - nw36 - nw37 = 0
+Aeq(3,[7,9,18,19,20,21]) = [1,1,-1,-1,-1,-1];
+%s1 = np11 + np12
+%s1 - np11 - np12 = 0
+Aeq(4,[22,1,2]) = [1,-1,-1];
+%s2 = np21 + np22
+%s2 - np21 - np22 = 0
+Aeq(5,[23,3,4]) = [1,-1,-1];
+%s3 = np31 + np32 + np33
+%s3 - np31 - np32 - np33 = 0
+Aeq(6,[24,5,6,7]) = [1,-1,-1,-1];
+%s4 =        np42 + np43
+%s4 - np42 - np43 = 0
+Aeq(7,[25,8,9]) = [1,-1,-1];
+%d1 = nw11
+%d1 - nw11 = 0
+Aeq(8,[26,10]) = [1,-1];
+%d2 = nw12
+%d2 - nw12 = 0
+Aeq(9,[27,11]) = [1,-1];
+%d3 = nw13 + nw23
+%d3 - nw13 - nw23 = 0
+Aeq(10,[28,12,14]) = [1,-1,-1];
+%d4 = nw14 + nw24 + nw34
+%d4 - nw14 - nw24 - nw34 = 0
+Aeq(11,[29,13,15,18]) = [1,-1,-1,-1];
+%d5 =        nw25 + nw35
+%d5 - nw25 - nw35 = 0
+Aeq(12,[30,16,19]) = [1,-1,-1];
+%d6 =        nw26 + nw36
+%d6 - nw26 - nw36 = 0
+Aeq(13,[31,17,20]) = [1,-1,-1];
+%d7 =                      nw37
+%d7 - nw37 = 0
+Aeq(14,[32,21]) = [1,-1];
+
+% ------------------------------------------------------------
 % objective function vector
 %   note matlab arrays/vectors start at index 1 (not 0)
+% ------------------------------------------------------------
 f = zeros(32,1);
-f(1)  = 10    % np11
-f(2)  = 15    % np12
-f(3)  = 11    % np21
-f(4)  = 8     % np22
-f(5)  = 13    % np31
-f(6)  = 8     % np32
-f(7)  = 9     % np33
-f(8)  = 14    % np42
-f(9)  = 8     % np43
-f(10) = 5     % nw11
-f(11) = 6     % nw12
-f(12) = 7     % nw13
-f(13) = 10    % nw14
-f(14) = 12    % nw23
-f(15) = 8     % nw24
-f(16) = 10    % nw25
-f(17) = 14    % nw26
-f(18) = 14    % nw34
-f(19) = 12    % nw35
-f(20) = 12    % nw36
-f(21) = 6     % nw37
-f(22) = 0     % s1
-f(23) = 0     % s2
-f(24) = 0     % s3
-f(25) = 0     % s4
-f(26) = 0     % d1
-f(27) = 0     % d2
-f(28) = 0     % d3
-f(29) = 0     % d4
-f(30) = 0     % d5
-f(31) = 0     % d6
-f(32) = 0     % d7
+f(1)  = 10    % np11(value in f is cp11)
+f(2)  = 15    % np12(value in f is cp12)
+f(3)  = 11    % np21(value in f is cp21)
+f(4)  = 8     % np22(value in f is cp22)
+f(5)  = 13    % np31(value in f is cp31)
+f(6)  = 8     % np32(value in f is cp32)
+f(7)  = 9     % np33(value in f is cp33)
+f(8)  = 14    % np42(value in f is cp42)
+f(9)  = 8     % np43(value in f is cp43)
+f(10) = 5     % nw11(value in f is cw11)
+f(11) = 6     % nw12(value in f is cw12)
+f(12) = 7     % nw13(value in f is cw13)
+f(13) = 10    % nw14(value in f is cw14)
+f(14) = 12    % nw23(value in f is cw23)
+f(15) = 8     % nw24(value in f is cw24)
+f(16) = 10    % nw25(value in f is cw25)
+f(17) = 14    % nw26(value in f is cw26)
+f(18) = 14    % nw34(value in f is cw34)
+f(19) = 12    % nw35(value in f is cw35)
+f(20) = 12    % nw36(value in f is cw36)
+f(21) = 6     % nw37(value in f is cw37)
 
+% ------------------------------------------------------------
+% call solver and obtain solution
+% ------------------------------------------------------------
+[x fval] = linprog(f,A,b,Aeq,beq,lb,ub)
